f5bde140306ee0ae3bfa2979bb736871a58838a9
Vaddiarun <116086928+Vaddiarun@users.noreply.github.com>
2024-12-29 23:49:17 +0530

    adsa

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Root deployment
+  base: '/', // Keep this for root-level deployment
 });
