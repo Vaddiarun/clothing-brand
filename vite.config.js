8f72af516376f9a280bb0f2a2b7c0f42bec6fc62
Vaddiarun <116086928+Vaddiarun@users.noreply.github.com>
2024-12-30 07:25:58 +0530

    ssss

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Use '/' if deployed at the root, or '/subdirectory/' if in a subdirectory
+  base: '/jsv', // Use '/' if deployed at the root, or '/subdirectory/' if in a subdirectory
 });
